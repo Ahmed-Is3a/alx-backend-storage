f489cfcdc543a01ffcb9017d6fcb1b5f8752b2dd
ahmed <ahmedissaten@gmail.com>
2024-07-17 19:54:21 +0200

    save history

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,6 +20,19 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    '''store the history of inputs and outputs for a particular function'''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''wrap the decorated function and return the wrapper'''
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        return output
+    return wrapper
+
+
 class Cache:
     """
     connect to redis
@@ -32,6 +45,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         store data
