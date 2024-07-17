9d84e91f36516fffe6c162434b5b534a9a88506a
ahmed <ahmedissaten@gmail.com>
2024-07-17 09:28:12 +0200

    cache class

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Wrinting strings to redis
+"""
+import redis
+from uuid import uuid4
+from typing import Union
+
+
+class Cache:
+    """
+    connect to redis
+    """
+    def __init__(self):
+        """
+        create redis instatce
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        store data
+        """
+        rkey = str(uuid4())
+        self._redis.set(rkey, data)
+        return rkey
