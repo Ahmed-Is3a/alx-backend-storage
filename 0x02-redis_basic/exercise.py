7063c403dfb51939bd3ed402639d488fa17e7180
ahmed <ahmedissaten@gmail.com>
2024-07-17 15:51:50 +0200

    add get method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Wrinting strings to redis
 """
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Optional
 
 
 class Cache:
@@ -25,3 +25,23 @@ class Cache:
         rkey = str(uuid4())
         self._redis.set(rkey, data)
         return rkey
+
+    def get(self, key: str, fn: Optional[callable] = None) -> any:
+        """
+        get the value from redis
+        key: string
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fu(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """ get string """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """ get integer """
+        return self.get(key, fn=int)
